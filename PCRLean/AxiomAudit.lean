7fb7e10e1e01db50a2041ba9bfee88f57248c11f
Chenxiao Tian <62594979+Chenxiao-Tian@users.noreply.github.com>
2026-08-01 22:53:59 -0400

    PCR Lean: audit polynomial chart theorems

--- PCRLean/AxiomAudit.lean
+++ PCRLean/AxiomAudit.lean
@@ -6,4 +6,10 @@ import PCRLean
 #print axioms PCRLean.QuadraticDebt.collisionDebt_recurrence
 #print axioms PCRLean.QuadraticDebt.noFixedDirectionDepthBound
 #print axioms PCRLean.NoRecharge.consumed_remains_consumed
+#print axioms PCRLean.PolynomialCharts.oddCusp_sPivot_identity
+#print axioms PCRLean.PolynomialCharts.oddCusp_yPivot_identity
+#print axioms PCRLean.PolynomialCharts.tameQuadratic_active_identity
+#print axioms PCRLean.PolynomialCharts.artinSchreier_active_identity
+#print axioms PCRLean.PolynomialCharts.radicialRoot_active_identity
+#print axioms PCRLean.PolynomialCharts.squareCleaning_identity
 #print axioms PCRLean.certified_component_summary
