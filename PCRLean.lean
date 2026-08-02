6bd17d366df1e9be04a847cddf4d5a71af9c1f3c
Chenxiao Tian <62594979+Chenxiao-Tian@users.noreply.github.com>
2026-08-01 22:02:37 -0400

    PCR-Lean: import abstract certificate backend

--- PCRLean.lean
+++ PCRLean.lean
@@ -2,4 +2,5 @@ import PCRLean.Chambers.OddCusp
 import PCRLean.Chambers.RamifiedQuadratic
 import PCRLean.Chambers.ArtinSchreier
 import PCRLean.Termination.Generational
+import PCRLean.Framework.RankedSystem
 import PCRLean.Certified.CertifiedIndex
