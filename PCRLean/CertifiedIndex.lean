4dcdc82fe5a070e1819ab280a086dee1fe0f002b
Chenxiao Tian <62594979+Chenxiao-Tian@users.noreply.github.com>
2026-08-02 13:01:38 -0400

    PCR Lean: certify canonical Hasse saturation descent

--- PCRLean/CertifiedIndex.lean
+++ PCRLean/CertifiedIndex.lean
@@ -33,6 +33,7 @@ import PCRLean.ProductMatrixUnitGeneration
 import PCRLean.IteratedHasseProduct
 import PCRLean.IdealOperatorPacketDescent
 import PCRLean.MonogenicFrobeniusFrame
+import PCRLean.HasseSaturationDescent
 import PCRLean.SplitKernelCentre
 import PCRLean.FiniteKernelCentre
 import PCRLean.ProjectiveKernelCentre
