1b7968db1d560277e00be09575f3eef9bd113570
Chenxiao Tian <62594979+Chenxiao-Tian@users.noreply.github.com>
2026-08-02 01:32:39 -0400

    PCR Lean: include finite-packet split kernel centre

--- PCRLean/CertifiedIndex.lean
+++ PCRLean/CertifiedIndex.lean
@@ -14,6 +14,7 @@ import PCRLean.DifferentialOrbitKernel
 import PCRLean.DifferentialOrbitClosure
 import PCRLean.NoetherianOperatorOrbit
 import PCRLean.SplitKernelCentre
+import PCRLean.FiniteKernelCentre
 import PCRLean.NoetherianPatchingCompiler
 import PCRLean.CartierNoetherMaster
 import PCRLean.ActualIdealGluing
