1f0878d02305c460d71dbb4a279cc4328d345ec6
Chenxiao Tian <62594979+Chenxiao-Tian@users.noreply.github.com>
2026-08-01 21:50:04 -0400

    PCR-Lean: add library root

---
+++ PCRLean.lean
@@ -0,0 +1,5 @@
+import PCRLean.Chambers.OddCusp
+import PCRLean.Chambers.RamifiedQuadratic
+import PCRLean.Chambers.ArtinSchreier
+import PCRLean.Termination.Generational
+import PCRLean.Certified.CertifiedIndex
