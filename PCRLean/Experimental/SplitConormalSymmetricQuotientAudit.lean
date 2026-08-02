8da0e5ba1541f00cde3938206dfe292900e02e1a
Chenxiao Tian <62594979+Chenxiao-Tian@users.noreply.github.com>
2026-08-02 19:19:30 -0400

    PCR Jump Lean: update split-conormal quotient audit names

--- PCRLean/Experimental/SplitConormalSymmetricQuotientAudit.lean
+++ PCRLean/Experimental/SplitConormalSymmetricQuotientAudit.lean
@@ -1,6 +1,6 @@
 import PCRLean.Experimental.SplitConormalSymmetricQuotient
 
-#print axioms PCRLean.Experimental.SplitConormalSymmetricQuotient.keep_comp_section
+#print axioms PCRLean.Experimental.SplitConormalSymmetricQuotient.keep_comp_embedK
 #print axioms PCRLean.Experimental.SplitConormalSymmetricQuotient.ι_sub_projection_ι
 #print axioms PCRLean.Experimental.SplitConormalSymmetricQuotient.sub_projection_mem
 #print axioms PCRLean.Experimental.SplitConormalSymmetricQuotient.transverseIdeal_le_ker
