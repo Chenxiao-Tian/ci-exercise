7c84717aa8b82538e7ec19bddc282d3a95d76464
Chenxiao Tian <62594979+Chenxiao-Tian@users.noreply.github.com>
2026-08-01 22:40:34 -0400

    PCR Lean: add kernel axiom audit

---
+++ PCRLean/AxiomAudit.lean
@@ -0,0 +1,9 @@
+import PCRLean
+
+#print axioms PCRLean.GenRank.wellFounded
+#print axioms PCRLean.GenStep.noInfinitePath
+#print axioms PCRLean.OddCusp.sPivotReenters
+#print axioms PCRLean.QuadraticDebt.collisionDebt_recurrence
+#print axioms PCRLean.QuadraticDebt.noFixedDirectionDepthBound
+#print axioms PCRLean.NoRecharge.consumed_remains_consumed
+#print axioms PCRLean.certified_component_summary
