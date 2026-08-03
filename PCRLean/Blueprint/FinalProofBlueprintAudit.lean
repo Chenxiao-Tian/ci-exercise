7ebacee5cab1d5c088cefe0693b9644b8472425f
Chenxiao Tian <62594979+Chenxiao-Tian@users.noreply.github.com>
2026-08-03 18:15:06 -0400

    MLEL-D001: add architecture blueprint axiom audit

---
+++ PCRLean/Blueprint/FinalProofBlueprintAudit.lean
@@ -0,0 +1,6 @@
+import PCRLean.Blueprint.FinalProofBlueprint
+
+#print axioms PCRLean.Blueprint.FinalProofAtlas.dep_rank_lt
+#print axioms PCRLean.Blueprint.FinalProofAtlas.depRel_wellFounded
+#print axioms PCRLean.Blueprint.FinalProofAtlas.blueprint_implies_final_claim
+#print axioms PCRLean.Blueprint.FinalProofAtlas.paperEnds_strictly_increase
